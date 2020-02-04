3d5235a499a423d6dc512761c3abd41ed45d5744
mitsuoysharag <mitsuoysharag@gmail.com>
2020-02-04 13:18:23 -0500

    deploy page

--- deploy-project.sh
+++ deploy-project.sh
@@ -1,13 +1,3 @@
 git add .
 git commit -m 'deploy project'
 git push origin master
-
-
-
-# ng build --prod --baseHref https://mitsuoysharag.github.io/beinplace-admin/
-# cd dist/beinplace-admin
-
-# git init
-# git add -A
-# git commit -m 'deploy page'
-# git push -f https://github.com/mitsuoysharag/beinplace-admin.git master:gh-pages
