493efec629d35b21920f42b9f619404e6f05e4b5
mitsuoysharag <mitsuoysharag@gmail.com>
2020-02-23 23:11:49 -0500

    deploy project

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -1,7 +1,7 @@
 import { getSession } from './security'
 
-const service = 'http://localhost:5000'
-// const service = 'https://braintutor-service-v2.herokuapp.com'
+// const service = 'http://localhost:5000'
+const service = 'https://braintutor-service-v2.herokuapp.com'
 
 const headers = {
   'Content-Type': 'application/json',
