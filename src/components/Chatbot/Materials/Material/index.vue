0f549632faeab4a58d6bc30d8998d0a8da303cb9
mitsuoysharag <mitsuoysharag@gmail.com>
2020-06-18 17:54:14 -0500

    deploy project

--- src/components/Chatbot/Materials/Material/index.vue
+++ src/components/Chatbot/Materials/Material/index.vue
@@ -270,9 +270,9 @@ export default {
 }
 
 .item {
-  margin: 0 2px;
-  width: 20px;
-  height: 20px;
+  margin: 0 4px;
+  width: 22px;
+  height: 22px;
   // background: green;
   border-radius: 100%;
 
