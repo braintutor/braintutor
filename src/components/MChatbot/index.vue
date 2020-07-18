09ac423d253d3f5c5aefc0229f63f504f66a5e8f
mitsuoysharag <mitsuoysharag@gmail.com>
2020-07-18 16:10:56 -0500

    deploy project

--- src/components/MChatbot/index.vue
+++ src/components/MChatbot/index.vue
@@ -1,44 +1,158 @@
 <template>
-  <div class="chatbot" :class="{' chatbot--active': show}" @click="show = true">
-    <v-icon
-      v-if="show"
-      @click="$event.stopPropagation(); show = false"
-      class="chatbot__close"
-    >mdi-close</v-icon>
+  <div
+    class="chatbot"
+    :class="{' chatbot--active': show}"
+    onclick="input.focus()"
+    @click="showChatbot()"
+  >
+    <v-icon @click="hideChatbot($event)" class="chatbot__close">mdi-close</v-icon>
+    <!-- Avatar -->
     <div class="avatar">
       <img :src="require('@/assets/avatar/normal.png')" alt />
     </div>
-    <div class="messages messages--active"></div>
-    <div class="input">
-      <input type="text" v-model="new_message" placeholder="Mensaje" />
+    <!-- Messages -->
+    <div id="messages" class="messages messages--active">
+      <div
+        v-for="(message, idx) in messages"
+        :key="idx"
+        class="message"
+        :class="`message--${message.type}`"
+      >{{message.text}}</div>
+    </div>
+    <!-- Input -->
+    <form @submit.prevent="newMessage()" class="input">
+      <input
+        id="input"
+        type="text"
+        v-model="new_message"
+        placeholder="Escribe un mensaje"
+        autocomplete="off"
+      />
       <!-- <v-btn icon small>
         <v-icon small>mdi-send</v-icon>
       </v-btn>-->
-    </div>
+    </form>
   </div>
 </template>
 
 <script>
+import { scrollDown } from "@/services/scroll";
+import Chatbot from "@/services/chatbot";
+
 export default {
   data: () => ({
-    show: true,
-    new_message: ""
-  })
+    chatbot: null,
+    messages: [
+      {
+        text: "hola soy tu ayudante",
+        type: "bot"
+      },
+      {
+        text: "hola soy tu ayudante asdad sada dsad sadsa dsa sadas",
+        type: "user"
+      },
+      {
+        text:
+          "hola soy tu ayudante asdasd sadsa sad sad sadsa sadsa dsa dsadsa",
+        type: "bot"
+      },
+      {
+        text: "hola soy tu ayudante asdasd sa dsa sad",
+        type: "user"
+      },
+      {
+        text:
+          "hola soy tu ayudante asdasd sadsa sad sad sadsa sadsa dsa dsadsa",
+        type: "bot"
+      },
+      {
+        text: "hola soy tu ayudante asdasd sa dsa sad",
+        type: "user"
+      }
+    ],
+    new_message: "",
+    show: true
+  }),
+  mounted() {
+    this.chatbot = new Chatbot();
+    let knowledge = [
+      { questions: ["Hola"], answers: ["Hola @usuario@nombre."] },
+      {
+        questions: ["Adiós", "Chau", "Nos vemos"],
+        answers: ["Nos vemos pronto @usuario@nombre.", "Adiós @usuario@nombre."]
+      },
+      { questions: ["Gracias"], answers: ["No hay de qué."] }
+    ];
+    let entities = {
+      usuario: {
+        nombre: this.$store.state.user.first_name.split(/\s+/g)[0]
+      }
+    };
+    this.chatbot.train(knowledge, entities);
+  },
+  methods: {
+    newMessage() {
+      let answer = this.new_message;
+      this.addMessage(answer, "user");
+      this.new_message = "";
+
+      answer = this.getAnswer(answer);
+      this.chatbot.talk(answer);
+      this.addMessage(answer, "bot");
+    },
+    addMessage(text, type) {
+      this.messages.push({
+        text,
+        type
+      });
+      setTimeout(() => {
+        scrollDown("messages");
+      }, 20);
+    },
+    getAnswer(answer) {
+      let { answers } = this.chatbot.getAnswer(answer);
+      return answers[Math.floor(Math.random() * answers.length)];
+    },
+    //
+    showChatbot() {
+      this.show = true;
+      setTimeout(() => {
+        scrollDown("messages");
+      }, 500);
+    },
+    hideChatbot(e) {
+      e.stopPropagation();
+      this.show = false;
+    }
+  }
 };
 </script>
 
 <style lang='scss' scoped>
+$time: 0.3s;
+$icon-height: 90px;
+$icon-width: 90px;
+
 .chatbot {
+  height: $icon-height;
+  width: $icon-width;
+  max-height: 90vh;
+  max-width: 90vw;
   overflow: hidden;
+
+  background: #fff;
   border-radius: 50%;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
-  transition: 0.3s;
+  transition: $time;
   z-index: 10000;
   cursor: pointer;
 
   position: fixed;
   right: 20px;
   bottom: 20px;
+
+  display: flex;
+  flex-direction: column;
 
   &:hover {
     transform: translateY(-10px);
@@ -49,14 +163,26 @@ export default {
     position: absolute;
     top: 4px;
     right: 4px;
+    opacity: 0;
+    transition: $time;
+
+    color: #fff;
+    pointer-events: none;
 
     &:focus {
       outline: none;
     }
+
+    .chatbot--active & {
+      opacity: 1;
+      pointer-events: initial;
+    }
   }
 
   &--active {
-    border-radius: 0;
+    width: 350px;
+    height: 500px;
+    border-radius: 4px;
     cursor: initial;
 
     &:hover {
@@ -68,14 +194,14 @@ export default {
 
 .avatar {
   background: #7a7aff;
-  transition: 0.3s;
+  transition: $time;
 
   img {
     display: block;
-    width: 100px;
-    height: 100px;
+    width: $icon-width;
+    height: $icon-height;
     margin: 0 auto;
-    transition: 0.3s;
+    transition: $time;
   }
 
   .chatbot--active & {
@@ -87,34 +213,74 @@ export default {
 }
 
 .messages {
-  width: 0;
-  height: 0;
-  max-height: calc(100vh - 200px);
-  transition: 0.4s;
+  padding: 8px;
+  overflow-y: auto;
+  opacity: 0;
+  transition: $time;
 
   .chatbot--active & {
-    width: 350px;
-    height: 350px;
-    background: #fff;
+    flex-grow: 1;
+    opacity: 1;
+  }
+}
+
+.message {
+  width: max-content;
+  max-width: 80%;
+  padding: 4px 10px;
+  font-size: 0.85rem;
+  border-radius: 14px;
+  word-wrap: break-word;
+
+  &:not(:last-child) {
+    margin-bottom: 8px;
+  }
+
+  &--bot {
+    background: #e2e2e2;
+  }
+  &--user {
+    margin-left: auto;
+    background: #437bff;
+    color: #fff;
   }
 }
 
 .input {
   padding: 6px;
-  // border-top: 1px solid #ccc;
-  background: #fff;
+  transition: $time;
+  opacity: 0;
   display: flex;
 
   input[type="text"] {
     flex-grow: 1;
+    padding: 6px 14px;
 
-    padding: 5px 10px;
-    background: #dbdbdb;
-    font-size: .9rem;
+    background: #d1d1d1;
+    font-size: 0.85rem;
     border-radius: 20px;
+    transition: $time;
 
     &:focus {
       outline: none;
+    }
+  }
+
+  .chatbot--active & {
+    opacity: 1;
+  }
+}
+
+@media only screen and (max-width: 768px) {
+  .chatbot {
+    &--active {
+      height: 100%;
+      width: 100%;
+      max-height: initial;
+      max-width: initial;
+      bottom: 0;
+      right: 0;
+      border-radius: 0;
     }
   }
 }
