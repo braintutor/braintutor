cc2d25b3e96a3ec882bdf75bbf9e397dbe37e54e
mitsuoysharag <mitsuoysharag@gmail.com>
2020-09-17 00:31:35 -0500

    Chatbot: new UI

--- src/components/MChatbot/index.vue
+++ src/components/MChatbot/index.vue
@@ -364,11 +364,13 @@ $color-message-user: #0078ff;
   }
 
   .chatbot {
+    max-height: 100vh;
     max-width: 100vw;
-    width: 100%;
-    height: 100%;
+    width: auto;
+    height: auto;
     top: 56px;
     bottom: 0;
+    left: 0;
     right: 0;
     border-radius: 0;
   }
