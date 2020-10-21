99547ff17fc308e24e2a5d6cc1823a1a3847681c
Israel Mitsuo Yshara Garcia <mitsuoysharag@gmail.com>
2020-10-21 15:31:48 -0500

    Task: Add Public

--- src/components/TeacherSession/TasksEditor/index.vue
+++ src/components/TeacherSession/TasksEditor/index.vue
@@ -28,9 +28,15 @@
       :isPublic="task.public"
       :options="[
         {
-          text: 'Editar',
+          text: 'Editar Tarea',
           action: () => {
             showEdit(task);
+          },
+        },
+        {
+          text: 'Editar Fecha',
+          action: () => {
+            showEditDate(task);
           },
         },
         {
@@ -57,7 +63,7 @@
     </div>
 
     <!-- DIALOG NEW -->
-    <v-dialog v-model="dialog_new" persistent max-width="750">
+    <v-dialog v-model="dlg_new" persistent max-width="750">
       <form @submit.prevent="create()" class="m-card">
         <div class="m-card__body">
           <div>
@@ -89,7 +95,70 @@
             color="primary"
             class="mr-2"
             v-show="!loading_save"
-            @click="dialog_new = false"
+            @click="dlg_new = false"
+            >Cerrar</m-btn
+          >
+          <m-btn type="submit" small color="primary" :loading="loading_save"
+            >Guardar</m-btn
+          >
+        </div>
+      </form>
+    </v-dialog>
+    <!-- DIALOG EDIT -->
+    <v-dialog v-model="dlg_edit" persistent max-width="750">
+      <form @submit.prevent="save()" class="m-card">
+        <div class="m-card__body">
+          <v-text-field
+            v-model="task.title"
+            :maxlength="TaskModel.title.max_length"
+            :counter="TaskModel.title.max_length"
+            label="Título"
+            required
+          ></v-text-field>
+          <v-textarea
+            v-model="task.description"
+            :maxlength="TaskModel.description.max_length"
+            :counter="TaskModel.description.max_length"
+            label="Descripción"
+            required
+          ></v-textarea>
+          <v-checkbox v-model="task.public" label="Público"></v-checkbox>
+        </div>
+        <div class="m-card__actions">
+          <m-btn
+            type="button"
+            small
+            text
+            color="primary"
+            class="mr-2"
+            v-show="!loading_save"
+            @click="dlg_edit = false"
+            >Cerrar</m-btn
+          >
+          <m-btn type="submit" small color="primary" :loading="loading_save"
+            >Guardar</m-btn
+          >
+        </div>
+      </form>
+    </v-dialog>
+    <!-- DIALOG EDIT DATE -->
+    <v-dialog v-model="dlg_edit_date" persistent max-width="450">
+      <form @submit.prevent="saveDate()" class="m-card">
+        <div class="m-card__body">
+          <div>
+            <span class="mr-2">Tiempo de Inicio:</span>
+            <input type="datetime-local" v-model="task.time_start_f" required />
+          </div>
+        </div>
+        <div class="m-card__actions">
+          <m-btn
+            type="button"
+            small
+            text
+            color="primary"
+            class="mr-2"
+            v-show="!loading_save"
+            @click="dlg_edit_date = false"
             >Cerrar</m-btn
           >
           <m-btn type="submit" small color="primary" :loading="loading_save"
@@ -99,7 +168,7 @@
       </form>
     </v-dialog>
     <!-- DIALOG REMOVE -->
-    <v-dialog v-model="dialog_remove" max-width="400">
+    <v-dialog v-model="dlg_remove" max-width="400">
       <div class="m-card">
         <div class="m-card__body">
           <h3>¿Eliminar la Tarea?</h3>
@@ -108,12 +177,12 @@
           </p>
         </div>
         <div class="m-card__actions">
-          <m-btn @click="dialog_remove = false" color="primary" small
+          <m-btn @click="dlg_remove = false" color="primary" small
             >Cancelar</m-btn
           >
           <m-btn
             @click="
-              dialog_remove = false;
+              dlg_remove = false;
               remove();
             "
             color="error"
@@ -161,14 +230,16 @@ export default {
     action: "",
     //
     loading_save: false,
-    dialog_new: false,
-    dialog_remove: false,
+    dlg_new: false,
+    dlg_edit: false,
+    dlg_edit_date: false,
+    dlg_remove: false,
     TaskModel,
     variables,
   }),
   async created() {
     this.session_id = getParam("session_id");
-    this.restore();
+    this.init();
   },
   computed: {
     tasks_ordered() {
@@ -176,60 +247,67 @@ export default {
     },
   },
   methods: {
-    showCreate() {
-      this.action = "create";
-      this.task = {
-        time_start_f: this._formatDateToInput(new Date().addHours(1)),
-        public: false,
-      };
-      this.dialog_new = true;
-    },
-    showEdit(task) {
-      this.action = "edit";
-      this.task = {
-        ...task,
-        id: task._id,
-        time_start_f: this._formatDateToInput(task.time_start),
-      };
-      this.dialog_new = true;
-    },
-    showRemove(task) {
-      this.task = Object.assign({}, task);
-      this.task.id = this.task._id;
-      this.dialog_remove = true;
-    },
-    showAnswers(task) {
-      this.task_selected = Object.assign({}, task);
+    async init() {
+      this.showLoading("Cargando Tareas");
+      try {
+        this.students = this.mongoArr(
+          await getStudentsBySession(this.session_id)
+        );
+        this.tasks = this.mongoArr(
+          await getTasksBySessionTeacher(this.session_id)
+        );
+      } catch (error) {
+        this.showMessage("", error.msg || error);
+      }
+      this.hideLoading();
     },
     async create() {
       this.loading_save = true;
-      if (this.action === "create") {
-        try {
-          let _id = await addTask(this.session_id, {
-            ...this.task,
-            time_start: new Date(this.task.time_start_f),
-          });
-          this.task._id = _id.$oid;
-          this.task.time_start = new Date(this.task.time_start_f);
-          this.tasks.push(this.task);
-          this.dialog_new = false;
-        } catch (error) {
-          this.showMessage("", error.msg || error);
-        }
-      } else if (this.action === "edit") {
-        try {
-          await this.$api.task.update(this.task.id, {
-            ...this.task,
-            time_start: new Date(this.task.time_start_f),
-          });
-          this.task.time_start = new Date(this.task.time_start_f);
-          let task_idx = this.tasks.findIndex((t) => t._id === this.task.id);
-          this.tasks[task_idx] = Object.assign({}, this.task);
-          this.tasks.splice();
-          this.dialog_new = false;
-        } catch (error) {
-          this.showMessage("", error.msg || error);
-        }
+      try {
+        let _id = await addTask(this.session_id, {
+          ...this.task,
+          time_start: new Date(this.task.time_start_f),
+        });
+        this.task._id = _id.$oid;
+        this.task.time_start = new Date(this.task.time_start_f);
+        this.tasks.push(this.task);
+        this.dlg_new = false;
+      } catch (error) {
+        this.showMessage("", error.msg || error);
+      }
+      this.loading_save = false;
+    },
+    async save() {
+      this.loading_save = true;
+      try {
+        await this.$api.task.update(this.task._id, {
+          ...this.task,
+        });
+        let task_idx = this.tasks.findIndex((t) => t._id === this.task._id);
+        this.tasks[task_idx] = {
+          ...this.tasks[task_idx],
+          ...this.task,
+        };
+        this.tasks.splice();
+        this.dlg_edit = false;
+      } catch (error) {
+        this.showMessage("", error.msg || error);
+      }
+      this.loading_save = false;
+    },
+    async saveDate() {
+      this.loading_save = true;
+      try {
+        await this.$api.task.update(this.task._id, {
+          time_start: new Date(this.task.time_start_f),
+        });
+        this.task.time_start = new Date(this.task.time_start_f);
+        let task_idx = this.tasks.findIndex((t) => t._id === this.task._id);
+        this.tasks[task_idx].time_start = this.task.time_start;
+        this.tasks.splice();
+        this.dlg_edit_date = false;
+      } catch (error) {
+        this.showMessage("", error.msg || error);
       }
       this.loading_save = false;
     },
@@ -244,19 +322,36 @@ export default {
       }
       this.hideLoading();
     },
-    async restore() {
-      this.showLoading("Cargando Tareas");
-      try {
-        this.students = this.mongoArr(
-          await getStudentsBySession(this.session_id)
-        );
-        this.tasks = this.mongoArr(
-          await getTasksBySessionTeacher(this.session_id)
-        );
-      } catch (error) {
-        this.showMessage("", error.msg || error);
-      }
-      this.hideLoading();
+    showCreate() {
+      this.task = {
+        time_start_f: this._formatDateToInput(new Date().addHours(1)),
+        public: false,
+      };
+      this.dlg_new = true;
+    },
+    showEdit(task) {
+      this.task = {
+        _id: task._id,
+        title: task.title,
+        description: task.description,
+        public: task.public,
+      };
+      this.dlg_edit = true;
+    },
+    showEditDate(task) {
+      this.task = {
+        _id: task._id,
+        time_start_f: this._formatDateToInput(task.time_start),
+      };
+      this.dlg_edit_date = true;
+    },
+    showRemove(task) {
+      this.task = Object.assign({}, task);
+      this.task.id = this.task._id;
+      this.dlg_remove = true;
+    },
+    showAnswers(task) {
+      this.task_selected = Object.assign({}, task);
     },
     //
     _formatDateToInput(date) {
