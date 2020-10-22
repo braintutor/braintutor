2e3484e2b13650c5342677a6e0f2e472af71a5ab
Israel Mitsuo Yshara Garcia <mitsuoysharag@gmail.com>
2020-10-22 13:53:03 -0500

    Task: Fixed Public

--- src/models/Question.js
+++ src/models/Question.js
@@ -2,7 +2,7 @@ import variables from "./variables";
 
 const Question = {
   question: {
-    max_length: 500,
+    max_length: 1000,
   },
   alternatives: {
     max_length: variables.max_alternatives_per_question,
