b19e2786c6a3a85d31ea88fc83b2e8d44210cf12
Israel Mitsuo Yshara Garcia <mitsuoysharag@gmail.com>
2020-11-16 14:00:58 -0500

    Report: Add Observations

--- src/views/DirectorReports.vue
+++ src/views/DirectorReports.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="m-container">
-    <div v-for="(course, idx) in courses" :key="idx" class="m-card mb-4">
+    <div
+      v-for="(course, idx) in courses"
+      :key="idx"
+      @click="showReports(course._id)"
+      class="m-card mb-4"
+    >
       <p>Curso: {{ course.name }}</p>
       <p v-if="course.teacher">
         Docente:
         {{ `${course.teacher.last_name}, ${course.teacher.first_name}` }}
       </p>
-
-      <div>
-        <p>Reportes:</p>
-        <div>{{ course.reports }}</div>
-      </div>
     </div>
   </div>
 </template>
@@ -21,17 +21,27 @@ export default {
     courses: [],
   }),
   async created() {
-    this.courses = this.mongoArr(await this.$api.course.getAll());
-
-    for (const course of this.courses) {
-      let reports = this.mongoArr(
-        await this.$api.report.getAll({
-          course_id: course._id,
-        })
-      );
-      course.reports = reports;
+    this.showLoading("Cargando Cursos");
+    try {
+      this.courses = this.mongoArr(await this.$api.course.getAll());
+    } catch (error) {
+      this.showMessage("", error.msg || error);
     }
-    this.$forceUpdate();
+    this.hideLoading();
+  },
+  methods: {
+    async showReports(course_id) {
+      this.showLoading("Cargando Reportes");
+      try {
+        let reports = this.mongoArr(
+          await this.$api.report.getAll({ course_id })
+        );
+        console.log(reports);
+      } catch (error) {
+        this.showMessage("", error.msg || error);
+      }
+      this.hideLoading();
+    },
   },
 };
 </script>
