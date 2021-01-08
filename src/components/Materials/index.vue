f45145df9bb226ad10577215c0ab4ee4622fa95b
artmadeit <this.arthur.email@gmail.com>
2021-01-08 12:07:03 -0500

    feat: show tooltip, improve ui

--- src/components/Materials/index.vue
+++ src/components/Materials/index.vue
@@ -61,9 +61,18 @@
                 >mdi-check</v-icon
               >
             </div>
-            <div class="d-flex justify-space-between">
+            <div class="material-name">
               <span>{{ m.name }}</span>
-              <v-icon v-if="m.is_private">mdi-eye-off-outline</v-icon>
+              <v-tooltip bottom v-if="m.is_private">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-icon 
+                    v-bind="attrs"
+                    v-on="on">
+                    mdi-eye-off-outline
+                  </v-icon>
+                </template>
+                <span>Privado</span>
+              </v-tooltip>
             </div>
           </section>
         </div>
@@ -456,6 +465,12 @@ export default {
     font-size: 1.5rem;
     margin: 16px 24px;
   }
+}
+
+.material-name {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
 }
 
 .link {
