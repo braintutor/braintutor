1788a64ab5cc069d4010862a68e0b5477d2e0ad6
artmadeit <this.arthur.email@gmail.com>
2021-01-19 01:03:33 -0500

    feat: add spanish

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,9 +1,14 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import DatetimePicker from 'vuetify-datetime-picker'
+import es from 'vuetify/es5/locale/es'
 
 Vue.use(Vuetify);
 Vue.use(DatetimePicker);
 
 export default new Vuetify({
+    lang: {
+        locales: { es },
+        current: 'es',
+    },
 });
