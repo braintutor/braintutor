875a6eaff09b0d6ea0aa04187277ed96b726513f
Dede1421 <mauricioandre91@gmail.com>
2021-01-21 18:39:29 -0500

    style: se mejoró datetime

--- src/components/globals/DateTime.vue
+++ src/components/globals/DateTime.vue
@@ -11,29 +11,42 @@
     <template slot="timeIcon">
       <v-icon>mdi-clock-outline</v-icon>
     </template>
+    <template slot="actions" slot-scope="{ parent }">
+      <!-- <v-btn small class="cancel-button" @click="parent.clearHandler"
+        >Cancelar</v-btn
+      > -->
+      <v-btn small color="primary" @click="parent.okHandler">Aceptar</v-btn>
+    </template>
   </v-datetime-picker>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            textFieldProps: {prependInnerIcon: 'mdi-calendar'}
-        }
+  data() {
+    return {
+      textFieldProps: { prependInnerIcon: "mdi-calendar" },
+    };
+  },
+  props: {
+    value: Date,
+    disabled: { type: Boolean, default: false },
+  },
+  computed: {
+    dateTime: {
+      get() {
+        return this.value;
+      },
+      set(dateTime) {
+        this.$emit("input", dateTime);
+      },
     },
-    props: {
-        value: Date,
-        disabled: { type: Boolean, default: false },
-        
-    },
-    computed: {
-        dateTime: {
-            get() { return this.value },
-            set(dateTime) {this.$emit('input', dateTime)}
-        }
-    }
-}
+  },
+};
 </script>
 
-<style>
+<style lang='scss' scoped>
+.cancel-button {
+  background: none;
+  border: 1px solid gray;
+}
 </style>
