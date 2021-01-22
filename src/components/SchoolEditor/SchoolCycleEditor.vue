b91f0d22db765d2d9a5f1d682708cca767d6e1d7
Dede1421 <mauricioandre91@gmail.com>
2021-01-22 13:35:39 -0500

    feat: ciclo escolar maqueta inicial

--- src/components/SchoolEditor/SchoolCycleEditor.vue
+++ src/components/SchoolEditor/SchoolCycleEditor.vue
@@ -1,13 +1,47 @@
 <template>
-  <div>hello!!</div>
+  <div class="editor">
+    <div class="editor__menu">
+      <div class="editor__title">
+        <h2>Ciclo académico escolar</h2>
+        <strong class="ml-2 mt-1" style="opacity: 0.5"></strong>
+      </div>
+      <m-btn color="primary" small>
+        <v-icon small class="mr-2">mdi-plus</v-icon>Crear
+      </m-btn>
+    </div>
+    <div class="editor__content m-4">
+      <table class="m-table">
+        <thead>
+          <tr>
+            <th class="text-left">Marzo 2021</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>Bimestre 2021</tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style lang="scss">
+.editor {
+  padding: 10px 16px;
+  &__menu {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  &__title {
+    display: flex;
+    align-items: center;
+  }
+  &__content {
+    overflow-x: auto;
+  }
+}
 </style>
