1d05cf5f3e865492c76164642bee5de61a20fe88
artmadeit <this.arthur.email@gmail.com>
2021-01-23 13:34:50 -0500

    feat: show current date hint

--- src/components/SchoolEditor/SchoolCycleSegments.vue
+++ src/components/SchoolEditor/SchoolCycleSegments.vue
@@ -9,11 +9,14 @@
         <h4>{{ text }} {{ segment.number }}</h4>
       </v-col>
       <v-col>
-
-        <date-input label="Inicio"  v-model="segment.start" />
+        <date-input label="Inicio" 
+          :date-picker-props="{'show-current': showCurrent(segment)}" 
+          v-model="segment.start" />
       </v-col>
       <v-col>
-        <date-input label="Fin" :date-picker-props="{'min': segment.start}" v-model="segment.end" />
+        <date-input label="Fin" 
+          :date-picker-props="{'min': segment.start, 'show-current': segment.start}" 
+          v-model="segment.end" />
       </v-col>
     </v-row>
   </v-container>
@@ -39,6 +42,15 @@ export default {
       },
     },
   },
+  methods: {
+    showCurrent(segment) {
+      const segmentIndex = this.segments.indexOf(segment)
+      if(segmentIndex == 0) return true // if first return current date
+
+      const lastSegment = this.segments[segmentIndex - 1]
+      return lastSegment.end
+    }
+  }
 };
 </script>
 
