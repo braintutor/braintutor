bce2d9dcd092fc9a40adb51d9fd0b52887402784
diana <qpdiam@gmail.com>
2021-02-03 14:00:55 -0500

    feat: show time recording

--- src/modules/SchoolClass/SchoolClass.vue
+++ src/modules/SchoolClass/SchoolClass.vue
@@ -2,8 +2,13 @@
   <div class="m-container">
     <v-card v-for="(item, idx) in classes" :key="idx" class="mx-auto my-12">
       <v-card-title class="time"
-        >{{ item.start }} - {{ item.end }}
+        >Clase del dia {{ formatDate(item.start) }}
       </v-card-title>
+      <v-card-text v-if="item.recording">
+        Inicio: {{ formatHour(item.recording.startTime) }} 
+        <p> </p>
+        Fin: {{ formatHour(item.recording.endTime) }}
+      </v-card-text>
       <v-card-actions>
         <v-btn
           v-if="item.recording"
@@ -26,6 +31,7 @@
 <script>
 import { getAll } from "./service";
 import { getParam, redirect } from "@/services/router.js";
+import { format } from "date-fns";
 
 export default {
   data: () => ({
@@ -53,6 +59,14 @@ export default {
   },
   computed: {},
   methods: {
+    formatDate(dateString) {
+      const date = new Date(dateString);
+      return format(date, "dd/MM");
+    },
+    formatHour(timestamp) {
+      const date = new Date(parseInt(timestamp));
+      return format(date, "HH:m");
+    },
     seeRecord({ url }) {
       redirect(url);
     },
