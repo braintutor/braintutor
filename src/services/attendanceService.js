3c77f9009304ef0c522667f7fe5bc6e09d79a9dd
artmadeit <this.arthur.email@gmail.com>
2021-02-08 13:16:31 -0500

    feat(attendance): use new api

--- src/services/attendanceService.js
+++ src/services/attendanceService.js
@@ -1,8 +1,5 @@
-// import { fetch_get } from "./fetch";
+import { fetch_get } from "./fetch";
 
-export async function getAttendanceRecords() {
-    return [
-        { student: "Arthur", status: "absence" },
-        { student: "Diana", status: "attended" }
-    ]
+export function getAttendanceRecords() {
+    return fetch_get(`api/v1/attendance`)
 }
