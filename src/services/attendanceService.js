36b715741d033f902a9c59f622e3d9d54547864b
artmadeit <this.arthur.email@gmail.com>
2021-02-10 11:48:34 -0500

    feat(attendance): mark attendace api

--- src/services/attendanceService.js
+++ src/services/attendanceService.js
@@ -1,16 +1,13 @@
-import { fetch_get } from "./fetch";
+import { fetch_get, fetch_put } from "./fetch";
 
 export function getAttendanceRecords(classId) {
     return fetch_get(`api/v1/attendance/${classId}`)
 }
 
-function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-export async function markAttendance(classId, attendanceRecordId, newStatus) {
-    // return fetch_put(`api/v1/attendance/${classId}`)
-    await timeout(3000);
-    console.log(classId, attendanceRecordId, newStatus)
-    // throw "ahhh error 500"
+export function markAttendance(classId, studentId, newStatus) {
+    return fetch_put(`api/v1/attendance`, {
+        student_id: studentId,
+        school_class_id: classId,
+        new_status: newStatus
+    })
 }
