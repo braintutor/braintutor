87596cd3494bb4b9baf898819cfd2c7e4e63cbc5
artmadeit <this.arthur.email@gmail.com>
2021-02-18 18:37:04 -0500

    fix: color blank

--- src/services/eventService.js
+++ src/services/eventService.js
@@ -31,14 +31,14 @@ function removeEvent(event_id) {
   });
 }
 
-var randomColor = require('randomcolor');
+// var randomColor = require('randomcolor');
 
-function generateColor(text){
-  return text.length > 20? randomColor({
-    seed: text
-  }) : text
+// function generateColor(text){
+//   return text.length > 20? randomColor({
+//     seed: text
+//   }) : text
 
-}
+// }
 
 function getEvents(query){
   return fetch_post('event2', query)
