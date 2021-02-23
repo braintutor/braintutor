281036cddbef0f8b3d303aabb785897cd47038f2
artmadeit <this.arthur.email@gmail.com>
2021-02-23 12:24:03 -0500

    feat: call api resetPassword

--- src/services/resetPasswordService.js
+++ src/services/resetPasswordService.js
@@ -1,6 +1,9 @@
-// import { fetch_post } from "./fetch";
+import { fetch_post, fetch_put } from "./fetch";
 
 export function requestResetPassword(email) {
-    return Promise.resolve({email}).then(() => new Promise(resolve => setTimeout(resolve, 1000)))
-    // return fetch_post('api/v1/auth/reset-password', { email })
+    return fetch_post('api/v1/auth/reset-password', { email })
+}
+
+export function resetPassword(password, token) {
+    return fetch_put('api/v1/auth/reset-password-confirm', { password, token })
 }
--- src/views/NewPassword.vue
+++ src/views/NewPassword.vue
@@ -1,21 +1,35 @@
 <template>
   <div class="m-container">
     <h2>Cambio de contraseña</h2>
-    <p>Hola Arthur</p>
-    <v-form>
+    <!-- <p>Hola Arthur</p> -->
+    <v-form @submit.prevent="newPassword">
       <v-text-field type="password" v-model="password" placeholder="Ingrese su nueva contraseña" filled rounded dense></v-text-field>
-      <v-text-field type="password" v-model="confirmPassword" placeholder="confirme su nueva contraseña" filled rounded dense></v-text-field>
+      <v-text-field type="password" v-model="confirmPassword" placeholder="Confirme su nueva contraseña" filled rounded dense></v-text-field>
       <v-btn type="submit" color="primary">Guardar contraseña</v-btn>      
     </v-form>
   </div>
 </template>
 
 <script>
+import { resetPassword } from "@/services/resetPasswordService";
+
 export default {
   data(){
     return{
       password: "",
-      confirmPassword: ""
+      confirmPassword: "",
+      token: ""
+    }
+  },
+  methods: {
+    async newPassword() {
+      this.showLoading("Enviando correo");
+      try {
+        await resetPassword(this.password, this.token)
+      } catch (error) {
+        this.showMessage("", error.msg || error);
+      }
+      this.hideLoading();
     }
   }
 };
