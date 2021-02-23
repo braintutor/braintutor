6dfa98d50b6436042d935ad47de51966b2dab702
artmadeit <this.arthur.email@gmail.com>
2021-02-23 11:47:34 -0500

    feat: add status to reset password

--- src/views/ResetPassword.vue
+++ src/views/ResetPassword.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="m-container">
-    <h2>¿Cómo deseas restablecer tu contraseña?</h2>
-    <p>Te enviaremos un correo electrónico a tu cuenta</p>
-    <v-form @submit.prevent="resetPassword">
-      <v-text-field class="text-f" v-model="email" placeholder="Correo" type="email" filled rounded dense :rules="emailRules"></v-text-field>
-      <v-btn type="submit"  color="primary">Enviar</v-btn>
-    </v-form>
+    <div v-if="sended">
+      <p>Enviado</p>
+    </div>
+    <div v-else>
+      <h2>¿Cómo deseas restablecer tu contraseña?</h2>
+      <p>Te enviaremos un correo electrónico a tu cuenta</p>
+      <v-form @submit.prevent="resetPassword">
+        <v-text-field class="text-f" v-model="email" placeholder="Correo" type="email" filled rounded dense :rules="emailRules"></v-text-field>
+        <v-btn type="submit"  color="primary" :loading="loading">Enviar</v-btn>
+      </v-form>
+    </div>
   </div>
 </template>
 <script>
@@ -13,15 +18,20 @@ import { requestResetPassword } from "@/services/resetPasswordService";
 
 export default {
     data() {
-        return {
-            email: "",
-            emailRules: [v => !v || /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'Correo invalido'],
-         }
+      return {
+        status: "loading",
+        email: "",
+        emailRules: [v => !v || /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'Correo invalido'],
+      }
     },
     methods: {
       async resetPassword() {
-          await requestResetPassword(this.email)
+        await requestResetPassword(this.email)
       }
+    },
+    computed: {
+      loading() { return this.status === 'loading' },
+      sended() { return this.status === 'sended' }
     }
 }
 </script>
