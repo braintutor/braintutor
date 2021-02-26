a878a4670b4aaff329d3d2a5d363ad44c543ed10
diana <qpdiam@gmail.com>
2021-02-26 11:37:19 -0500

    feat: student detail view

---
+++ src/views/StudentDetail.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <p>Info del hijo: </p>
+       <Student v-if="false" :student="student_selected" />
+  </div>
+</template>
+
+<script>
+import Student from "../components/globals/Students/Student";
+
+export default {
+    components: { Student }
+
+}
+</script>
+
+<style>
+
+</style>
