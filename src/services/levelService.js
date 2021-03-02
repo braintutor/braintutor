1575220136ae38fe3b9c9730fbd8a52b38d629b9
artmadeit <this.arthur.email@gmail.com>
2021-03-02 10:41:21 -0500

    refactor: extract levels to a getLevels()

---
+++ src/services/levelService.js
@@ -0,0 +1,12 @@
+export async function getLevels() {
+    return [
+        {
+            _id: "PRI",
+            name: "Primaria",
+        },
+        {
+            _id: "SEC",
+            name: "Secundaria",
+        },
+    ]
+}
